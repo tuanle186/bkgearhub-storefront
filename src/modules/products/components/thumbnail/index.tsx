74919f0cfd5dbc63f27ef2bf14be0bf81b20a57e
Lê Quốc Tuấn <lequoctuan.qt@gmail.com>
2024-11-04 00:06:46 +0700

    Edit product thumbnail

--- src/modules/products/components/thumbnail/index.tsx
+++ src/modules/products/components/thumbnail/index.tsx
@@ -54,7 +54,7 @@ const ImageOrPlaceholder = ({
     <Image
       src={image}
       alt="Thumbnail"
-      className="absolute inset-0 object-cover object-center"
+      className="absolute inset-0 p-4 object-cover object-center"
       draggable={false}
       quality={50}
       sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
